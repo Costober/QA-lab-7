73fd775fe547483da64420ab8d45412a757c4c5c
Costober <162455632+Costober@users.noreply.github.com>
2025-11-16 12:58:38 +0200

    Update custom.test.js

--- tests/e2e/custom.test.js
+++ tests/e2e/custom.test.js
@@ -10,7 +10,7 @@ test('Перевірка заголовка сторінки "Про нас"', a
 test('Перевірка головного заголовка H1 на сторінці "Про нас"', async ({ page }) => {
   await page.goto('/about.html');
   const header = page.locator('h1');
-  await expect(header).toHaveText('Сторінка "Про нас"');
+  await expect(header).toHaveText('Неправильна сторінка "Про нас"');
 });
 
 // Тест 3: Перевірка навігації (посилання "Повернутися на головну")
@@ -18,4 +18,5 @@ test('Перевірка посилання на головну сторінку
   await page.goto('/about.html');
   await page.click('#homeLink');
   await expect(page).toHaveURL('http://localhost:3000/index.html');
+
 });
